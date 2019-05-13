9a489babadcdd72a859e4ffa9d0235c585b250b4
S-H-GAMELINKS <gamelinks007@gmail.com>
2019-05-13 16:58:52 +0900

    add get_apps

--- lib/arukas/api.rb
+++ lib/arukas/api.rb
@@ -1,8 +1,18 @@
+require "net/http"
+require "uri"
+
 module Arukas
     class API
         def initialize(token, secret)
             @arukas_token = token
             @arukas_secret = secret
+        end
+
+        def get_apps
+            puts @arukas_token
+            puts @arukas_secret
+            uri = URI.parse("https://#{@arukas_token}:#{@arukas_secret}@app.arukas.io/api/apps/")
+            response = Net::HTTP.get_response(uri)
         end
     end
 end
