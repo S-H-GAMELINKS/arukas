4b0e75141fbd80fd156c5184f4d4af10dc07d18e
S-H-GAMELINKS <gamelinks007@gmail.com>
2019-05-13 17:26:41 +0900

    using setup method

--- test/arukas_test.rb
+++ test/arukas_test.rb
@@ -4,16 +4,20 @@ require "dotenv"
 Dotenv.load
 
 class ArukasTest < Minitest::Test
+
+  def setup
+    @arukas = Arukas::API::new(ENV["ARUKAS_JSON_API_TOKEN"], ENV["ARUKAS_JSON_API_SECRET"])
+  end
+
   def test_that_it_has_a_version_number
     refute_nil ::Arukas::VERSION
   end
 
   def test_init_arukas
-    refute_nil Arukas::API::new(ENV["ARUKAS_JSON_API_TOKEN"], ENV["ARUKAS_JSON_API_SECRET"])
+    refute_nil @arukas
   end
 
   def test_get_arukas_apps
-    refute_nil @arukas = Arukas::API::new(ENV["ARUKAS_JSON_API_TOKEN"], ENV["ARUKAS_JSON_API_SECRET"])
     res = @arukas.get_apps
     assert @arukas.get_apps != nil
   end
