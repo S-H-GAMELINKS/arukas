22f03e33342c38619e578dbf49c1cac2df58e6ca
S-H-GAMELINKS <gamelinks007@gmail.com>
2019-05-13 17:02:25 +0900

    fix init arukas

--- lib/arukas/api.rb
+++ lib/arukas/api.rb
@@ -4,15 +4,11 @@ require "uri"
 module Arukas
     class API
         def initialize(token, secret)
-            @arukas_token = token
-            @arukas_secret = secret
+            @uri = URI.parse("https://#{token}:#{secret}@app.arukas.io/api/apps/")
         end
 
         def get_apps
-            puts @arukas_token
-            puts @arukas_secret
-            uri = URI.parse("https://#{@arukas_token}:#{@arukas_secret}@app.arukas.io/api/apps/")
-            response = Net::HTTP.get_response(uri)
+            Net::HTTP.get_response(@uri)
         end
     end
 end
