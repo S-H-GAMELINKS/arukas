4a7ecc8b94e2578d8dd7a1815d6d9fc9c513227f
S-H-GAMELINKS <gamelinks007@gmail.com>
2019-05-14 16:59:49 +0900

    fix url

--- lib/arukas/api.rb
+++ lib/arukas/api.rb
@@ -4,23 +4,23 @@ require "json"
 module Arukas
     class API
         def initialize(token, secret)
-            @url = "https://#{token}:#{secret}@app.arukas.io/api/apps/"
+            @url = "https://#{token}:#{secret}@app.arukas.io/api/"
         end
 
         def get_apps
-            RestClient.get @url
+            RestClient.get @url + "apps/"
         end
 
         def create_apps(params)
-            RestClient.post @url, payload=params, {:content_type => :json}
+            RestClient.post @url + "apps/", payload=params, {:content_type => :json}
         end
 
         def get_app(id)
-            RestClient.get @url + "#{id}"
+            RestClient.get @url + "apps/#{id}"
         end
 
         def delete_app(id)
-            RestClient.delete @url + "#{id}"
+            RestClient.delete @url + "apps/#{id}"
         end
     end
 end
